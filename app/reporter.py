c80b60cd60a85bb4b9f5253b86eda62d0eae1aa8
minipele06 <aja398@stern.nyu.edu>
2020-06-04 17:58:23 -0400

    with pandas df

--- app/reporter.py
+++ app/reporter.py
@@ -4,6 +4,20 @@ import os
 
 import pandas
 
+csv_filepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "..", "data", "monthly_sales.csv")
+
+df = pandas.read_csv(csv_filepath)
+
+best_seller = df.groupby(["product"]).sum()
+
+best_seller = best_seller.sort_values(by=["sales price"], ascending=False)
+
+sales = df.to_dict("records")
+
+#best_seller = best_seller.to_dict("records")
+
+#best_seller_sorted = sorted(best_seller, key=lambda x: x["sales price"])
+
 def to_usd(my_price):
     """
     Converts a numeric value to usd-formatted string, for printing and display purposes.
@@ -16,4 +30,16 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
-print("GENERATING SALES REPORT FOR MONTH OF OCTOBER 2013...")
+total_sales = 0
+
+for x in sales:
+    total_sales += x["sales price"]
+
+print("SALES REPORT (NOVEMBER 2018)")
+print("")
+print(f"TOTAL SALES: {to_usd(total_sales)}")
+print("")
+print("TOP SELLING PRODUCTS")
+print(best_seller.columns[0])
+print(f"1. {to_usd(best_seller['sales price'][0])}")
+
