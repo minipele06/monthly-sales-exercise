c7894e09ceff9d57bc6fdb84d6c39f892c0ede7b
minipele06 <aja398@stern.nyu.edu>
2020-06-04 19:04:11 -0400

    code optimzation

--- app/reporter.py
+++ app/reporter.py
@@ -4,42 +4,24 @@ import os
 
 import pandas
 
-csv_filepath = os.path.join(os.path.dirname(os.path.dirname(__file__)), "..", "data", "monthly_sales.csv")
-
-df = pandas.read_csv(csv_filepath)
-
-best_seller = df.groupby(["product"]).sum()
-
-best_seller = best_seller.sort_values(by=["sales price"], ascending=False)
-
-sales = df.to_dict("records")
-
-#best_seller = best_seller.to_dict("records")
-
-#best_seller_sorted = sorted(best_seller, key=lambda x: x["sales price"])
-
 def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
-
-    Param: my_price (int or float) like 4000.444444
-    
-    Example: to_usd(4000.444444)
-    
-    Returns: $4,000.44
-    """
     return f"${my_price:,.2f}" #> $12,000.71
 
-total_sales = 0
+csv_filepath = os.path.join((os.path.dirname(__file__)), "..", "data", "monthly_sales.csv")
 
-for x in sales:
-    total_sales += x["sales price"]
+sales = pandas.read_csv(csv_filepath)
+
+product_totals = sales.groupby(["product"]).sum()
+
+product_totals = product_totals.sort_values("sales price", ascending=False)
+
+total_sales = sales["sales price"].sum()
 
 print("SALES REPORT (NOVEMBER 2018)")
 print("")
 print(f"TOTAL SALES: {to_usd(total_sales)}")
 print("")
 print("TOP SELLING PRODUCTS")
-print(best_seller.columns[0])
-print(f"1. {to_usd(best_seller['sales price'][0])}")
+for x in range(0,3):
+    print(f"{x+1}. {product_totals.index[x]}: {to_usd(product_totals['sales price'][x])}")
 
